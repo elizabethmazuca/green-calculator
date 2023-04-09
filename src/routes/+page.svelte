24edd9f2c68eaf7582de58b519c583964ab9829e
Your Name <mazucachu@gmai.com>
2023-04-08 21:26:27 -0700

    website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<div class="container">
+	<ul class="nav">
+		<li>
+			<a class="navlink" href="/">Home</a>
+		</li>
+		<li>
+			<a class="navlink" href="page1.html">Page 1</a>
+		</li>
+		<li>
+			<a class="navlink" href="page2.html"> Page 2</a>
+		</li>
+	</ul>
+	<h1 class="title">Plastic Decomposition Facts</h1>
+	<p class="pictureCaption">Here's a picture</p>
+
+	<img class="oceantrash" src="./assets/oceantrash.png" alt="Ocean Trash" />
+
+    function increment () 
+    {
+        count = 0
+        $count++;
+    }
+	<button on:click={increment}> Fruit Snacks </button>
+</div>
